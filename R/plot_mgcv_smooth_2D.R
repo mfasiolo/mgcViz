ef679ad76074980da1559390cad6b128d99b90aa
Raphael Nedellec <raphael.nedellec@edf.fr>
2017-07-18 15:07:37 +0200

    extra spacing + work in progress

--- R/plot_mgcv_smooth_2D.R
+++ R/plot_mgcv_smooth_2D.R
@@ -4,27 +4,52 @@
 #' @name plot.mgcv.smooth.2D
 #' @examples 
 #' library(mgcv)
+#' library(mgcViz)
+#' library(viridis)
 #' set.seed(2) ## simulate some data...
 #' dat <- gamSim(1, n = 1000, dist = "normal", scale = 2)
 #' b <- gam(y ~ s(x0) + s(x1, x2) + s(x3), data = dat, method = "REML")
-#' b <- getViz(b)
-#' sm <- b(2)
-#' plot(sm, rug = TRUE, residuals = TRUE, scheme = 0)
-#' plot(sm, rug = TRUE, residuals = TRUE, scheme = 1)
+#' plot(b, scheme = 0, select = 2, se = FALSE)
+#' plot(b, scheme = 0, select = 2, se = TRUE)
+#' plot(b, scheme = 1, select = 2)
+#' plot(b, scheme = 2, select = 2)
+#' mgcv::plot.gam(b, scheme = 3, select = 2, se = TRUE)
+#' mgcv::plot.gam(b, scheme = 3, select = 2, se = FALSE)
+#' plot(b, scheme = 4, select = 2)
+#' sm <- getViz(b)(2)
+#' plot(sm, rug = TRUE, se = FALSE, residuals = TRUE, scheme = 0)
+#' plot(sm, rug = TRUE, se = TRUE, residuals = TRUE, scheme = 0)
+#' plot(sm, rug = TRUE, se = TRUE, residuals = TRUE, scheme = 1)
 #' plot(sm, rug = TRUE, residuals = TRUE, scheme = 2)
 #' plot(sm, rug = TRUE, residuals = TRUE, scheme = 3)
 #' @rdname plot.mgcv.smooth.2D
 #' @export plot.mgcv.smooth.2D
+
+# 
+# ll <- list(o = sm, residuals = FALSE, rug = TRUE, se = TRUE, n = 40,
+#            pers = FALSE, theta = 30, phi = 30, xlab = NULL, ylab = NULL, main=NULL,
+#            ylim = NULL, xlim = NULL, too.far = 0.1, se.mult = 1, shift = 0, trans = I, seWithMean = FALSE,
+#            unconditional = FALSE, by.resids = FALSE, scheme = 0, hcolors = viridis(50, begin=0.2),
+#            contour.col = 1, pFun = zto1(0.05, 3, 0.2))
+# list2env(ll, .GlobalEnv)
+# .createP <- mgcViz:::.createP
+# .initializeXXX <- mgcViz:::.initializeXXX
+# .plot.mgcv.smooth.2D <- mgcViz:::.plot.mgcv.smooth.2D
+# .spContour <- mgcViz:::.spContour
+
 plot.mgcv.smooth.2D <- function(o, residuals = FALSE, rug = TRUE, se = TRUE, n = 40,
-                                pers = FALSE, theta = 30, phi = 30, xlab = NULL, ylab = NULL, main=NULL, 
-                                ylim = NULL, xlim = NULL, too.far = 0.1, se.mult = 1, shift = 0, trans = I, seWithMean = FALSE, 
-                                unconditional = FALSE, by.resids = FALSE, scheme = 0, hcolors = viridis(50, begin=0.2),
-                                contour.col = 1, pFun = zto1(0.05, 3, 0.2), ...)
-  
-{
+                                pers = FALSE, theta = 30, phi = 30, xlab = NULL, ylab = NULL,
+                                main = NULL, ylim = NULL, xlim = NULL, too.far = 0.1, se.mult = 1,
+                                shift = 0, trans = I, seWithMean = FALSE, 
+                                unconditional = FALSE, by.resids = FALSE,
+                                scheme = 0, hcolors = viridis(50, begin = 0.2),
+                                contour.col = 1, pFun = zto1(0.05, 3, 0.2), ...) {
   if (length(scheme) > 1){ 
     scheme <- scheme[1]
-    warning( "scheme should be a single number" )
+    warning("'scheme' should be a single number")
+  }
+  if (!(scheme %in% 0:4)){
+    stop("'scheme' must be in 0:4")
   }
   o$smooth <- o$gObj$smooth[[o$ism]]
   resDen <- "none"
@@ -47,48 +72,66 @@ plot.mgcv.smooth.2D <- function(o, residuals = FALSE, rug = TRUE, se = TRUE, n =
                   ylim = ylim, xlim = xlim, too.far = too.far, shade = NULL, shade.col = NULL,
                   se1.mult = se.mult, se2.mult = se.mult, shift = shift, trans = trans,
                   by.resids = by.resids, scheme = scheme, seWithMean = seWithMean, fitSmooth = fv.terms,
-                  w.resid = w.resid, resDen = resDen, ...)
+                  w.resid = w.resid, resDen = resDen)#, ...)
   pd <- tmp[["P"]]
   attr(o$smooth, "coefficients") <- tmp[["coef"]]
   rm(tmp)
   # Plotting
   .ggobj <- .plot.mgcv.smooth.2D(x = o$smooth, P = pd, partial.resids = partial.resids,
                                  rug = rug, se = se, scale = FALSE, n2 = n,
-                                 pers = pers, theta = theta, phi = phi, jit = jit,
+                                 pers = pers, theta = theta, phi = phi, jit = NULL,
                                  main = main, too.far = too.far, 
                                  shift = shift, trans = trans, by.resids = by.resids,
                                  scheme = scheme, hcolors = hcolors,
-                                 contour.col = contour.col, pFun = pFun, ...)
-  .ggobj <- .ggobj + theme_bw()
-  attr(.ggobj, "rawData") <- pd
-  .ggobj
+                                 contour.col = contour.col, pFun = pFun, ...)#, ...)
+  if (inherits(.ggobj, "ggplot")) {
+    .ggobj <- .ggobj + theme_bw()
+    attr(.ggobj, "rawData") <- pd
+    return(.ggobj)
+  } else {
+    return(invisible(.ggobj))
+  }
+  
 }
 
+# ll2 <- list(x = o$smooth, P = pd, partial.resids = partial.resids,
+#             rug = rug, se = se, scale = FALSE, n2 = n,
+#             pers = pers, theta = theta, phi = phi, jit = NULL,
+#             main = main, too.far = too.far, 
+#             shift = shift, trans = trans, by.resids = by.resids,
+#             scheme = scheme, hcolors = hcolors,
+#             contour.col = contour.col, pFun = pFun)
+# list2env(ll2, .GlobalEnv)
+
 # Internal function
-.plot.mgcv.smooth.2D <- function(x, P = NULL, partial.resids = FALSE, rug = TRUE, se=TRUE, scale = FALSE, n2 = 40,
-                                 pers = FALSE, theta = 30, phi = 30, jit = FALSE, main = NULL, too.far = 0.1,
-                                 shift = 0, trans = I, by.resids = FALSE, scheme = 0, hcolors = viridis(50, begin = 0.2),
+.plot.mgcv.smooth.2D <- function(x, P = NULL, partial.resids = FALSE, rug = TRUE, se = TRUE,
+                                 scale = FALSE, n2 = 40,
+                                 pers = FALSE, theta = 30, phi = 30, jit = FALSE,
+                                 main = NULL, too.far = 0.1,
+                                 shift = 0, trans = I, by.resids = FALSE, scheme = 0,
+                                 hcolors = viridis(50, begin = 0.2),
                                  contour.col = 1, pFun = zto1(0.05, 3, 0.2), 
                                  # Useless arguments
                                  data = NA, label = NA, se.mult = NA, xlab = NA, ylab = NA, n = NA,
                                  shade = NA, shade.col = NA, xlim = NA, ylim = NA,
                                  #
-                                 ...)
-{
-  
+                                 ...) {
+  force(P)
   if (se) {
     P$fit[P$exclude] <- NA
-    if (pers) scheme <- 1
+    if (pers) {
+      scheme <- 1
+    }
     if (scheme == 1) { ## perspective plot 
-      persp(P$x, P$y, matrix(trans(P$fit+shift), n2, n2), xlab = P$xlab, ylab = P$ylab,
+      persp(P$x, P$y, matrix(trans(P$fit + shift), n2, n2), xlab = P$xlab, ylab = P$ylab,
             zlab = P$main, ylim = P$ylim, xlim = P$xlim, theta = theta, phi = phi, ...)
     } else if (scheme == 2 || scheme == 3) {
       if (scheme == 3) {
-        hcolors <- grey(0:50/50)
+        hcolors <- grey(0:50 / 50)
       }
       .pl <- 
-        ggplot(data = data.frame("z"= P$fit, "x" = rep(P$x, length(P$fit) / length(P$x)), 
-                                 "y"= rep(P$y, each = length(P$fit) / length(P$x)),
+        ggplot(data = data.frame("z" = P$fit, "x" = rep(P$x, length(P$fit) / length(P$x)), 
+                                 "y" = rep(P$y, each = length(P$fit) / length(P$x)),
                                  p = pFun(1 - pnorm(abs(P$fit) / P$se))), 
                aes(x = x, y = y, z = z)) +
         geom_raster(aes(fill = z, alpha = p)) + 
@@ -111,7 +154,7 @@ plot.mgcv.smooth.2D <- function(o, residuals = FALSE, rug = TRUE, se = TRUE, n =
     } else { ## contour plot with error contours
       .spContour(P$x, P$y, matrix(P$fit, n2, n2), matrix(P$se, n2, n2),
                  xlab = P$xlab, ylab = P$ylab, zlab = P$main, titleOnly =! is.null(main),
-                 se.mult = 1, trans = trans, shift = shift,...)
+                 se.mult = 1, trans = trans, shift = shift, ...)
       if (rug) { 
         if (is.null(list(...)[["pch"]])) {
           points(P$raw$x, P$raw$y, pch = ".", ...) 
@@ -124,16 +167,16 @@ plot.mgcv.smooth.2D <- function(o, residuals = FALSE, rug = TRUE, se = TRUE, n =
     P$fit[P$exclude] <- NA
     if (!is.null(main)) {
       P$title <- main
-      }
+    }
     if (pers) {
       scheme <- 1
-      }
+    }
     if (scheme == 1) { 
       persp(P$x, P$y, matrix(trans(P$fit + shift), n2, n2), xlab = P$xlab, ylab = P$ylab,
             zlab = P$main, theta = theta, phi = phi, xlim = P$xlim, ylim = P$ylim, ...)
     } else if (scheme == 2 || scheme == 3) {
       if (scheme == 3) {
-        hcolors <- grey(0:50/50)
+        hcolors <- grey(0:50 / 50)
       }
       image(P$x, P$y, matrix(trans(P$fit + shift), n2, n2), xlab = P$xlab, ylab = P$ylab,
             main = P$main, xlim = P$xlim, ylim = P$ylim, col = hcolors, ...)
@@ -141,9 +184,9 @@ plot.mgcv.smooth.2D <- function(o, residuals = FALSE, rug = TRUE, se = TRUE, n =
       if (rug) {  
         if (is.null(list(...)[["pch"]])) {
           points(P$raw$x, P$raw$y, pch = ".", ...)
-          } else {
-            points(P$raw$x, P$raw$y, ...)
-          }
+        } else {
+          points(P$raw$x, P$raw$y, ...)
+        }
       }
     } else { 
       contour(P$x, P$y, matrix(trans(P$fit + shift), n2, n2), xlab = P$xlab, ylab = P$ylab,
@@ -151,11 +194,15 @@ plot.mgcv.smooth.2D <- function(o, residuals = FALSE, rug = TRUE, se = TRUE, n =
       if (rug) {  
         if (is.null(list(...)[["pch"]])) {
           points(P$raw$x, P$raw$y, pch = ".", ...)
-          } else {
-            points(P$raw$x, P$raw$y, ...)
-          }
+        } else {
+          points(P$raw$x, P$raw$y, ...)
+        }
       }
     }
   }
-  return(.pl)
+  if (scheme > 1) {
+    return(.pl)
+  } else {
+    return(invisible(P))
+  }
 }
