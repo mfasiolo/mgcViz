702107c1687130b26b7c967da76f8e9774a0fd1e
Raphael Nedellec <raphael.nedellec@edf.fr>
2017-07-24 14:49:58 +0200

    spacing + more comments

--- R/plot_mgcv_smooth_1D.R
+++ R/plot_mgcv_smooth_1D.R
@@ -40,6 +40,10 @@
 #' b <- bam(y ~ s(x1)+s(x2), data = dat, method = "fREML", discrete = TRUE)
 #' v <- getViz(b)
 #' plot(v(1), rug = TRUE, resDen = "cond", residuals = TRUE)
+#' plot(v(1), rug = FALSE, resDen = "cond", residuals = TRUE)
+#' plot(v(1), rug = TRUE, resDen = "none", residuals = FALSE)
+#' plot(v(1), rug = TRUE, resDen = "cond", residuals = TRUE)
+#' plot(v(1), xlab = "XLAB", ylab = "YLAB", main = "TEST", shade = TRUE)
 #' @rdname plot.mgcv.smooth.1D
 #' @export plot.mgcv.smooth.1D
 plot.mgcv.smooth.1D <- function(o, residuals = FALSE, rug = TRUE, se = TRUE, n = 100, maxpo = 1e4,
@@ -50,18 +54,12 @@ plot.mgcv.smooth.1D <- function(o, residuals = FALSE, rug = TRUE, se = TRUE, n =
                                 scheme = 0, resDen = "none", ngr = c(50, 50), bw = NULL, tol = 1e-6, alpDen = 0.7, 
                                 dTrans = NULL, paletteDen = viridis(50, begin=0.2), alpha.rug = 1, ...)
 {
-  if (length(scheme)>1){ 
+  if (length(scheme) > 1){ 
     scheme <- scheme[1]
     warning( "scheme should be a single number" )
   }
-  
   resDen <- match.arg(resDen, c("none", "cond", "joint"))
-  
   o$smooth <- o$gObj$smooth[[o$ism]]
-  
-  # This creates/modifies variables in the environment.
-  # INPUTS: unconditional, o, residuals, se, resDen 
-  # NEW/MODIFIED VARIABLES: o, w.resid, partial.resids, se2.mult, se1.mult, se, fv.terms, order 
   fv.terms <- o$store$termsFit[ , o$store$np + o$ism]
   init <- .initializeXXX(o, unconditional, residuals, resDen, se, fv.terms)
   # affect initialize output
@@ -73,7 +71,6 @@ plot.mgcv.smooth.1D <- function(o, residuals = FALSE, rug = TRUE, se = TRUE, n =
   se <- init$se
   fv.terms <- init$fv.terms
   order <- init$order
-  
   # Prepare for plotting
   tmp <- .createP(sm = o$smooth, x = o$gObj, partial.resids = partial.resids,
                   se = se, n = n, n2 = NULL,
@@ -93,70 +90,95 @@ plot.mgcv.smooth.1D <- function(o, residuals = FALSE, rug = TRUE, se = TRUE, n =
                                  shift = shift, trans = trans, by.resids = by.resids, resDen = resDen, ngr = ngr,
                                  bw = bw, tol = tol, alpDen = alpDen, 
                                  dTrans = dTrans, paletteDen = paletteDen, alpha.rug = alpha.rug, ...)
-  
   .ggobj <- .ggobj + theme_bw() +
     theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
-  
   attr(.ggobj, "rawData") <- pd
-  .ggobj
+  return(.ggobj)
 }
 
-
 # Internal function for plotting one dimensional smooths
-.plot.mgcv.smooth.1D <- function(x, P, partial.resids=FALSE, rug=TRUE, se=TRUE, scale=TRUE, n=100,
-                                 maxpo = 1e4, jit=FALSE, shade=FALSE, shade.col=I("gray80"), ylim = NULL,
-                                 shift=0, trans=I, by.resids=FALSE, scheme=0, resDen="none", 
+.plot.mgcv.smooth.1D <- function(x, P, partial.resids = FALSE, rug = TRUE,
+                                 se = TRUE, scale = TRUE, n = 100, maxpo = 1e4,
+                                 jit = FALSE, shade = FALSE, shade.col = I("gray80"),
+                                 ylim = NULL, shift = 0, trans = I, by.resids = FALSE,
+                                 scheme = 0, resDen = "none", alpha.rug = 1,
                                  ngr = c(50, 50), bw = NULL, tol = 1e-6, alpDen = 0.7, 
-                                 dTrans = NULL, paletteDen = viridis(50, begin=0.2), alpha.rug = 1, ...)
-{
-  if (scheme == 1){ shade <- TRUE }
-  
+                                 dTrans = NULL, paletteDen = viridis(50, begin = 0.2), ...) {
+  if (scheme == 1){
+    shade <- TRUE 
+  }
   ul <- P$fit + P$se ## upper CL
   ll <- P$fit - P$se ## lower CL  
-  
-  if (scale==FALSE && is.null(ylim)) { # Calculate ylim of plot
-    ylimit <- range(c(if(partial.resids||(resDen!="none")){ P$p.resid } else { P$fit }, if(se){ c(ul, ll) } else { c() }), na.rm = TRUE) 
+  if (scale == FALSE && is.null(ylim)) { # Calculate ylim of plot
+    ylimit <- range(c(
+      if (partial.resids || (resDen != "none")) {
+        P$p.resid 
+      } else {
+        P$fit 
+      }, 
+      if(se){     # if standard error included
+        c(ul, ll) # we add upper and lower limit to the ylim calculus
+      }), na.rm = TRUE) 
   }
+  ylimit <- if (is.null(ylim)) { # unless specified
+    ylimit + shift               # we add the shift to ylim
+  } else {
+    ylim 
+  }
+  # base plot
+  .pl <- ggplot(data    = 
+                  data.frame(x = P$x,                  # x values
+                             y = trans(P$fit + shift), # fitted + shift, after trans if necessary
+                             uci = trans(ul + shift),  # upper confidence bound + shift & trans
+                             lci = trans(ll - shift)), # lower confidence bound + shift & trans
+                mapping = aes(x = x, y = y)) + 
+    xlim(P$xlim[1], P$xlim[2]) +                       # xlim already calculated, from P
+    ylim(trans(ylimit[1]), trans(ylimit[2])) +         # ylim 
+    labs(title = P$main, x = P$xlab, y = P$ylab)       # add custom labels
   
-  ylimit <- if (is.null(ylim)){ ylimit <- ylimit + shift } else { ylim }
-  
-  .pl <- ggplot(data=data.frame(x=P$x, y=trans(P$fit+shift), uci=trans(ul+shift), lci=trans(ll-shift)), aes(x=x, y=y)) + 
-    xlim(P$xlim[1], P$xlim[2]) + ylim(trans(ylimit[1]), trans(ylimit[2])) + labs(title = P$main, x = P$xlab, y = P$ylab)
-  
-  if( resDen != "none" ){ # Plot conditional residual density
-    if( is.null(dTrans) ){ dTrans <- function(.x){ .x^(1/3) } }
-    
-    .datR <- cbind(P$raw, trans(P$p.resid+shift))
+  if(resDen != "none") { # Plot conditional residual density
+    if(is.null(dTrans)) {
+      dTrans <- function(.x) .x^(1/3)
+    }
+    .datR <- cbind(P$raw, trans(P$p.resid + shift))
     # Suppress warnings related to ngrid being too small relative to bw. Happens with big dataset.
     withCallingHandlers({
-      if( is.null(bw) ){ bw <- c(dpik(.datR[ , 1], range.x=P$xlim, gridsize=ngr[1]), 
-                                 dpik(.datR[ , 2], range.x=ylimit, gridsize=ngr[2])) }
-      
-      estXY <- bkde2D(.datR, range.x = list(P$xlim, ylimit), gridsize = ngr, bandwidth = bw)
-      
-      if( resDen == "cond" ){ # Calculate conditional density of residuals | x
-        estXY$fhat <- estXY$fhat / bkde(.datR[ , 1], gridsize = ngr[1], range.x = P$xlim, bandwidth = bw[1])$y 
+      if(is.null(bw)) {
+        bw <- c(dpik(.datR[, 1], range.x = P$xlim, gridsize = ngr[1]), 
+                dpik(.datR[, 2], range.x = ylimit, gridsize = ngr[2]))
       }
-    }, warning = function(w) { invokeRestart("muffleWarning") })
-    
-    estXY$fhat[ estXY$fhat <= tol*dnorm(0, 0, sd(.datR[ , 2])) ] <- NA 
-    
-    cden <- data.frame("d" = sqrt(as.numeric(t(estXY$fhat))), 
-                       "x" = rep(estXY$x1, each=ngr[1]), 
-                       "y" = rep(estXY$x2, ngr[2]))
-    .pl <- .pl + geom_raster(data=cden, aes(x=x, y=y, fill=d), inherit.aes=FALSE, alpha = alpDen) + 
-      scale_fill_gradientn(colours = paletteDen, na.value="white")
+      estXY <- bkde2D(.datR, range.x = list(P$xlim, ylimit), gridsize = ngr, bandwidth = bw)
+      if(resDen == "cond") { # Calculate conditional density of residuals | x
+        estXY$fhat <- estXY$fhat / bkde(.datR[, 1], gridsize = ngr[1],
+                                        range.x = P$xlim, bandwidth = bw[1])$y 
+      }
+    }, warning = function(w) invokeRestart("muffleWarning"))
+    estXY$fhat[estXY$fhat <= tol * dnorm(0, 0, sd(.datR[, 2]))] <- NA 
+    .pl <- .pl + 
+      geom_raster(data = data.frame("d" = sqrt(as.numeric(t(estXY$fhat))), 
+                                    "x" = rep(estXY$x1, each = ngr[1]), 
+                                    "y" = rep(estXY$x2, ngr[2])),
+                  mapping = aes(x = x, y = y, fill = d),
+                  inherit.aes = FALSE, alpha = alpDen) + 
+      scale_fill_gradientn(colours  = paletteDen,
+                           na.value = "white")
   }
-  
   # Add shade or lines for confidence bands
-  if(se){
-    if( shade ){ 
-      .pl <- .pl + geom_polygon(data = data.frame("x"= c(P$x,P$x[n:1],P$x[1]), "y" = trans(c(ul,ll[n:1],ul[1])+shift)), 
-                                aes(x = x, y = y, fill = shade.col), inherit.aes = F)
+  if (se) {
+    if (shade) { 
+      .pl <- .pl +
+        geom_polygon(data = data.frame("x" = c(P$x, P$x[n:1], P$x[1]),
+                                       "y" = trans(c(ul, ll[n:1], ul[1]) + shift)), 
+                     mapping = aes(x = x, y = y, fill = shade.col),
+                     inherit.aes = FALSE)
     } else {
-      .tmpF <- function(pl, ..., linetype="dashed") # Alter default "linetype"
+      .tmpF <- function(pl, ..., linetype = "dashed") # Alter default "linetype"
       {
-        pl <- pl + geom_line(aes(x, uci), linetype=linetype, ...) + geom_line(aes(x, lci), linetype=linetype, ...)
+        pl <- pl +
+          geom_line(mapping  = aes(x = x, y = uci),
+                    linetype = linetype, ...) + # upper ci line
+          geom_line(mapping  = aes(x = x, y = lci),
+                    linetype = linetype, ...)   # lower ci line
       }
       .pl <- .tmpF(.pl, ...)
     }
@@ -169,30 +191,33 @@ plot.mgcv.smooth.1D <- function(o, residuals = FALSE, rug = TRUE, se = TRUE, n =
   }
   
   # Add partial residuals
-  if (partial.resids&&(by.resids||x$by=="NA")) { 
-    if (length(P$raw)==length(P$p.resid)) {
+  if (partial.resids && (by.resids | x$by == "NA")) { 
+    if (length(P$raw) == length(P$p.resid)) {
       .tmpF <- function(..., shape = '.', col = "black") # Alter default shape and col
       {
         geom_point(data = .datRes, 
-                   aes(x = resx, y = resy), na.rm = TRUE, shape = shape, col = col, ...)
+                   mapping = aes(x = resx, y = resy),
+                   na.rm = TRUE,
+                   shape = shape,
+                   col = col, ...)
       }
       .pl <- .pl + .tmpF(...)
     } else {
       warning("Partial residuals do not have a natural x-axis location for linear functional terms")
     }
   }
-  
   # Add rug
   if (rug) { 
     if( jit ){ .datRes$resx <- jitter(.datRes$resx) }
     .tmpF <- function(pl, ..., size = 0.2) # Alter default "size"
     {
-      geom_rug(data = .datRes, aes(x = resx), inherit.aes = F, size = size, alpha = alpha.rug, ...)
+      geom_rug(data = .datRes, 
+               mapping = aes(x = x),
+               inherit.aes = FALSE, size = size, alpha = alpha.rug,...)
     }
     .pl <- .pl + .tmpF(.pl, ...)
   } 
-  
+  # ??
   .pl <- .pl + geom_line(...)
-  
   return( .pl )
 } 
