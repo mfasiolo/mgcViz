d0e91d4d9febd01af8749b6483562e49b88208dc
Raphael Nedellec <raphael.nedellec@edf.fr>
2017-08-01 15:35:36 +0200

    cleaning function + exmpls + spacing

--- R/plot_fs_interaction_1D.R
+++ R/plot_fs_interaction_1D.R
@@ -4,6 +4,7 @@
 #' @name plot.fs.interaction.1D
 #' @examples 
 #' library(mgcv)
+#' library(mgcViz)
 #' set.seed(0)
 #' ## simulate data...
 #' f0 <- function(x) 2 * sin(pi * x)
@@ -22,67 +23,65 @@
 #' 
 #' ## fit model (note p-values not available when fit 
 #' ## using gamm)...
-#' bm <- gamm(y~s(x0)+ s(x1,fac,bs="fs",k=5)+s(x2,k=20))
+#' bm <- gamm(y ~ s(x0)+ s(x1, fac, bs = "fs", k = 5) + s(x2, k = 20))
 #' v <- getViz(bm$gam)
-#' plot( v(2) )
+#' plot(v(2))
+#' plot(v(2), ylim = c(-0.5, 0.5))
 #' @rdname plot.fs.interaction.1D
 #' @export plot.fs.interaction.1D
-plot.fs.interaction.1D <- function(o, n=100, alpha=NULL, legend=TRUE, xlab=NULL, ylab=NULL, main=NULL, ylim=NULL, xlim=NULL,
-                                   shift=0, trans=I, ...)
-{
-
+#' 
+plot.fs.interaction.1D <- function(o, n = 100, alpha = NULL, legend = TRUE,
+                                   xlab = NULL, ylab = NULL, main = NULL,
+                                   ylim = NULL, xlim = NULL,
+                                   shift = 0, trans = I, ...) {
   o$smooth <- o$gObj$smooth[[o$ism]]
- 
-  unconditional<-se<-residuals<-FALSE
-  # This creates/modifies variables in the environment.
-  # INPUTS: unconditional, o, residuals, se, resDen 
-  # NEW/MODIFIED VARIABLES: o, w.resid, partial.resids, se2.mult, se1.mult, se, fv.terms, order 
+  unconditional <- se <- residuals <- FALSE
   resDen <- "none"
   fv.terms <- o$store$termsFit[ , o$store$np + o$ism]
   init <- .initializeXXX(o, unconditional, residuals, resDen, se, fv.terms)
-  # affect initialize output
-  o <- init$o
-  w.resid <- init$w.resid
-  partial.resids <- init$partial.resids
-  se2.mult <- init$se2.mult
-  se1.mult <- init$se1.mult
-  se <- init$se
-  fv.terms <- init$fv.terms
-  order <- init$order
-  
   # Prepare for plotting
-  tmp <- .createP(sm=o$smooth, x=o$gObj, partial.resids=partial.resids,
-                  rug=NULL, se=se, scale=FALSE, n=n, n2=NULL,
-                  pers=NULL, theta=NULL, phi=NULL, jit=NULL, xlab=xlab, ylab=ylab, main=main, label=term.lab,
-                  ylim=ylim, xlim=xlim, too.far=NULL, shade=NULL, shade.col=NULL,
-                  se1.mult=NULL, se2.mult=NULL, shift=shift, trans=trans,
-                  by.resids=NULL, scheme=NULL, seWithMean=NULL, fitSmooth=fv.terms,
-                  w.resid=w.resid, resDen=resDen, ...)
-  pd <- tmp[["P"]]
+  tmp <- .createP(sm = init$o$smooth, x = o$gObj, partial.resids = init$partial.resids,
+                  se = init$se, n = n, n2 = NULL,
+                  xlab = xlab, ylab = ylab, main = main, 
+                  ylim = ylim, xlim = xlim, too.far = NULL, 
+                  se1.mult = NULL, se2.mult = NULL, 
+                  seWithMean = NULL, fitSmooth = init$fv.terms,
+                  w.resid = init$w.resid, resDen = resDen, ...)
   attr(o$smooth, "coefficients") <- tmp[["coef"]]
-  rm(tmp)
-  
   # Plotting
-  .ggobj <- .plot.fs.interaction.1D(x=o$smooth, P=pd,  ylim = ylim, shift=shift, trans=trans, alpha=alpha, ...)
-  
-  .ggobj <- .ggobj+theme_bw()
-  if( !legend ){ .ggobj <- .ggobj + theme(legend.position="none")  } 
-  
-  attr(.ggobj, "rawData") <- pd
+  .ggobj <- .plot.fs.interaction.1D(x = o$smooth, P = tmp[["P"]], ylim = ylim,
+                                    shift = shift, trans = trans, alpha = alpha, ...)
+  if(!legend){
+    .ggobj <- .ggobj + theme(legend.position="none")
+  } 
+  attr(.ggobj, "rawData") <- tmp[["P"]]
   .ggobj
 }
 
 # Internal function
-.plot.fs.interaction.1D <- function(x, P=NULL, ylim=NULL, shift=0, trans=I, alpha=NULL, ...) {
-  
-  .dat <- data.frame("x"=rep(P$x,P$nf), "y"=trans(P$fit+shift), "id"=as.factor(rep(x$flev, each=P$n)))
-  
-  if( is.null(ylim) ){ ylim <- range(.dat$y) }
-  if( is.null(alpha) ){ alpha <- if(P$nf<10){1}else{if(P$nf<100){0.5}else{0.3}} }
-  
-  .pl <- ggplot(data=.dat, aes("x"=x, "y"=y, "colour"=id)) + geom_line(alpha=alpha) + 
-                labs(title = P$main, x = P$xlab, y = P$ylab) + ylim(ylim[1], ylim[2])
-  
-  return( .pl )
+.plot.fs.interaction.1D <- function(x, P = NULL, ylim = NULL, shift = 0,
+                                    trans = I, alpha = NULL, ...) {
+  .dat <- data.frame("x"  = rep(P$x, P$nf),
+                     "y"  = trans(P$fit + shift),
+                     "id" = as.factor(rep(x$flev, each = P$n)))
+  if (is.null(ylim)) {
+    ylim <- range(.dat$y) 
+  }
+  if (is.null(alpha)){
+    alpha <- 
+      if (P$nf < 10){
+        1
+      } else if (P$nf < 100) {
+        0.5
+      } else {
+        0.3
+      }
+  }
+  .pl <- 
+    ggplot(data = .dat, aes("x" = x, "y" = y, "colour" = id)) +
+    geom_line(alpha = alpha) + 
+    labs(title = P$main, x = P$xlab, y = P$ylab) +
+    ylim(ylim[1], ylim[2])
+  return(.pl)
 } ## end plot.fs.interaction
 
