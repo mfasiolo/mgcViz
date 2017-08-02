365ca3cbee9e727b97e8d56514cbcfd3e91815c9
Raphael Nedellec <raphael.nedellec@gmail.com>
2017-08-02 10:30:09 +0200

    add documentation argMaster & argSetup

--- R/I_argMaster.R
+++ R/I_argMaster.R
@@ -1,8 +1,10 @@
-
+#' @description `.argMaster`
+#' @param nam A character - a function name. One of `plot.mgcv.smooth.1D`, `plot.mgcv.smooth.2D`, 
+#' `plot.fs.interaction`.
+#' @return A list, with default options and graphical parameters.
+#' @noRd
 .argMaster <- function(nam){
-  
-  nam <- match.arg(nam, "plot.mgcv.smooth.1D")
-  
+  nam <- match.arg(nam, "plot.mgcv.smooth.1D", "plot.mgcv.smooth.2D", "plot.fs.interaction")
   out <- switch(nam,
                 "plot.mgcv.smooth.1D" = list(
                   "a.rug" = list(jit = FALSE, colour = "black", size = 0.2, alpha = 1), # rug layer
@@ -12,8 +14,8 @@
                   "a.res" = list(by.resids = FALSE, colour = "black", shape = 46, na.rm = TRUE), # residuals layer
                   "a.dens" = list(ngr = c(50, 50), bw = NULL, tol = 1e-6, alpDen = 0.7, 
                                   colours = viridis(50, begin = 0.2), na.value = "white") # density layer
-                ))
-  
-  return( out )
-  
+                ),
+                "plot.mgcv.smooth.2D" = list(),
+                "plot.fs.intercation" = list())
+  return(out)
 }
