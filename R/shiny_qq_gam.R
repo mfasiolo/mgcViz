9f093b4033c33a3e5fabbdad780637664c53a89c
Raphael Nedellec <raphael.nedellec@gmail.com>
2017-08-02 16:09:10 +0200

    shiny gadget proposal for zoom

--- R/shiny_qq_gam.R
+++ R/shiny_qq_gam.R
@@ -1,35 +1,113 @@
-#' Shiny Gadget for check.gam
-#'
-#' @param plots, Bla. 
-#' @return Bla.
-#' @export
+#' Shiny Gadget for qq_gam
+#' @description ...
 #' @import shiny
 #' @import miniUI
-#'
-#' @examples
-check_shiny.gam <- function(plots){
+#' @export
+shiny_qq_gam <- function(obj){
+  name_obj <- deparse(substitute(obj))
   ui <- miniPage(
-    gadgetTitleBar("Checking results"),
+    gadgetTitleBar("Q-Q GAM"),
     miniContentPanel(
-      # The brush="brush" argument means we can listen for
-      # brush events on the plot using input$brush.
-      fillRow(
-        fillCol(
-          plotOutput("plot1", height = "100%"),
-          plotOutput("plot2", height = "100%")
-        ),
-        fillCol(
-          plotOutput("plot3", height = "100%"),
-          plotOutput("plot4", height = "100%")
-        )
+      fillRow(flex = c(1, 4),
+              fillCol(
+                selectizeInput(
+                  inputId = "shape",
+                  label = "Point shape", choices = c(".", 1:25)),
+                colourpicker::colourInput(
+                  inputId = "color_line",
+                  label = "Line color",
+                  value = "red"
+                ),
+                selectizeInput(
+                  inputId = "ci",
+                  label = "Conf. Int. ?",
+                  choices = c(TRUE, FALSE),
+                  selected = "FALSE"
+                ),
+                colourpicker::colourInput(
+                  inputId = "color_CI",
+                  label = "CI color",
+                  value = "gray80"
+                ),
+                selectizeInput(
+                  inputId = "show_reps",
+                  label = "Show repetitions ?",
+                  choices = c(TRUE, FALSE),
+                  selected = "FALSE"
+                ),
+                colourpicker::colourInput(
+                  inputId = "color_rep",
+                  label = "Color for rep.",
+                  value = "black"
+                ),
+                sliderInput(
+                  inputId = "rep_alpha",
+                  label = "Alpha for rep.",
+                  min = 0, max = 1,
+                  step = 0.01,
+                  value = 0.05
+                )
+              ),
+              plotOutput("plot", height = "100%",
+                         dblclick = "plot_dblclick",
+                         brush = brushOpts(id = "plot_brush",
+                                           resetOnNew = TRUE))
       )
     )
   )
   server <- function(input, output, session) {
-    output$plot1 <- renderPlot(print(plots[[1]]))
-    output$plot2 <- renderPlot(print(plots[[2]]))
-    output$plot3 <- renderPlot(print(plots[[3]]))
-    output$plot4 <- renderPlot(print(plots[[4]]))
+    ranges <- reactiveValues(x = NULL, y = NULL)
+    qq <- qq.gam(obj)
+    shape <- reactive(
+      if (input$shape %in% as.character(1:25)) {
+        as.integer(input$shape)
+      } else {
+        input$shape
+      }
+    )
+    output$plot <- renderPlot(
+      zoom(qq, xlim = ranges$x, ylim = ranges$y,
+           shape = shape(),
+           CI = as.logical(input$ci),
+           show.reps = as.logical(input$show_reps),
+           rl.col = input$color_line,
+           ci.col = input$color_CI,
+           rep.col = input$color_rep,
+           rep.alpha = input$rep_alpha)
+    )
+    observeEvent(input$plot_dblclick, {
+      brush <- input$plot_brush
+      if (!is.null(brush)) {
+        ranges$x <- c(brush$xmin, brush$xmax)
+        ranges$y <- c(brush$ymin, brush$ymax)
+      } else {
+        ranges$x <- NULL
+        ranges$y <- NULL
+      }
+    })
+    observeEvent(input$done, {
+      if (rstudioapi::isAvailable()){
+        callText <- paste0(
+          "zoom(qq.gam(", name_obj, "), ",
+          ifelse(!is.null(ranges$x),
+                 sprintf("xlim = %s, ", deparse(round(ranges$x, 2))), ""),
+          ifelse(!is.null(ranges$y),
+                 sprintf("ylim = %s, ", deparse(round(ranges$y, 2))), ""),
+          "CI = ", input$ci, ", ",
+          "show.reps = ", input$show_reps, ", ",
+          "rep.col = \"", input$color_rep, "\", ",
+          "rep.alpha = ", input$rep_alpha, ", ",
+          "rl.col = \"", input$color_line, "\", ",
+          ifelse(is.character(shape()), "shape = \".\", ",
+                 sprintf("shape = %i, ", shape())),
+          "ci.col = \"", input$color_CI,
+          "\")")
+        rstudioapi::insertText(callText)
+      }
+      stopApp()
+    })
   }
-  runGadget(ui, server)
+  runGadget(ui, server, viewer = dialogViewer(dialogName = "Q-Q GAM",
+                                              height = 900, width = 900))
 }
+
