6b9f559cf8d61ad359af743a0bbf7e5661753e37
Jugurtha <matteo.fasiolo@gmail.com>
2017-08-05 16:12:36 +0100

    New residual.gam method, including `tunif` and `tnormal` options

---
+++ R/residuals_gam.R
@@ -0,0 +1,35 @@
+#' Generalized Additive Model residuals
+#' 
+#' @description Wrapper around \code{mgcv::residuals.gam}.
+#' @name residuals.gam
+#' @details ...
+#' @examples 
+#' library(mgcViz)
+#' 
+#' @rdname residuals.gam
+#' @export residuals.gam
+residuals.gam <- function(object, type = "deviance", ...)
+{
+  
+  o <- object
+
+  if( type %in% c("tunif", "tnormal") ){
+
+    fam <- fix.family.cdf( o$family )
+    
+    if( is.null(fam$cdf) ){ stop("CDF unavailable for this family: can't use `tunif` or `tnormal` type") }
+     
+    y <- fam$cdf(o$y, o$fitted.values, o$prior.weights, o$sig2, logp = TRUE)
+    
+    if( type == "tnormal" ) { y <- qnorm(y, log.p = TRUE) } else { y <- exp(y) } 
+    
+  } else {
+    
+    # NB `mgcv::` important to avoid an infinite recursion
+    y <- mgcv::residuals.gam(o, type = type, ...) 
+    
+  }
+  
+  return( y )
+  
+}
