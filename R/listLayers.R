64979ef383477b9768fc16ae1db8896323c8509a
Jugurtha <matteo.fasiolo@gmail.com>
2017-09-25 17:58:52 +0100

    Added function for listing available layers

---
+++ R/listLayers.R
@@ -0,0 +1,24 @@
+#'
+#' Lists available layer
+#'
+#' @param o an object.
+#' @rdname listLayers
+#' @export listLayers
+#' 
+listLayers <- function(o){
+  
+  cl <- paste(class(o), collapse = '')
+  
+  out <- switch(cl, 
+                "plotSmoothfs1D" = c("fitLine"),
+                "plotSmooth1D" = c("ciLine", "ciPoly", "fitLine", "resDens", "resPoints", "resRug"),
+                "plotSmooth2D" = c("fitLine", "fitRaster", "resDens", "resPoints", "resRug"), 
+                "plotSmoothMD" = c("fitLine", "fitRaster", "resDens", "resPoints", "resRug"), 
+                
+                "plotSmoothCheck1D" = c("denCheck", "gridCheck", "resDens", "resPoints", "resRug"), 
+                
+                message(paste("No layers for obj of this class", paste(class(o), collapse = ' '))))
+  
+  return(out)
+
+} 
