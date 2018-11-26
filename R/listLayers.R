2147d5f7d169bac723a68ad414ffc5ea3da6f693
Jugurtha <matteo.fasiolo@gmail.com>
2018-11-26 11:50:26 +0000

    Automated listLayer() function

--- R/listLayers.R
+++ R/listLayers.R
@@ -40,41 +40,16 @@ listLayers <- function(o){
   
   ty <- paste(o$type, collapse = '')
   
-  out <- switch(ty, 
-                # Smooth effects plots
-                "fs1D" = c("l_fitLine"),
-                "1D" = c("l_ciLine", "l_ciPoly", "l_fitLine", "l_dens", "l_points", "l_rug", "l_simLine", "l_fitDens"),
-                "Multi1D" = c("l_fitLine", "l_rug"),
-                "2D" = c("l_fitContour", "l_fitRaster", "l_dens", "l_points", "l_rug"), 
-                "MD" = c("l_fitContour", "l_fitRaster", "l_dens", "l_points", "l_rug"), 
-                "sos0" = c("l_fitContour", "l_fitRaster", "l_points", "l_rug", 
-                                       "l_coordContour", "l_bound"), 
-                "sos1" = c("l_fitContour", "l_fitRaster", "l_points", "l_rug"),
-                "randomEffect" = c("l_fitLine", "l_points", "l_ciLine", "l_ciPoly"),
-                "MultiRandomEffect" = c("l_points"),
-                "mrf" = c("l_poly"),
-                
-                # Slices of MD smooths
-                "MDslice" = c("l_fitContour", "l_fitRaster", "l_points", "l_rug"),
-                
-                # Parametric effects plots
-                "PtermNumeric" = c("l_ciLine", "l_ciPoly", "l_fitLine", "l_dens", 
-                                               "l_points", "l_rug"), 
-                "MultiPtermNumeric" = c("l_fitPoints", "l_fitBar", "l_ciBar"), 
-                "PtermFactor"  =  c("l_fitPoints", "l_fitBar", "l_ciBar", "l_points", "l_rug"),
-                "MultiPtermFactor"  =  c("l_fitPoints", "l_ciBar"),
-                "PtermLogical" = c("l_fitPoints", "l_fitBar", "l_ciBar", "l_points", "l_rug"),
-                
-                # Checking plots
-                "Check1DNumeric" = c("l_densCheck", "l_gridCheck1D", "l_gridQCheck1D", "l_dens", "l_points", "l_rug"),
-                "Check1DFactor" = c("l_gridCheck1D", "l_gridQCheck1D", "l_points", "l_rug"),
-                "Check1DLogical" = c("l_gridCheck1D", "l_gridQCheck1D", "l_points", "l_rug"),
-                "Check2DNumericNumeric" = c("l_gridCheck2D", "l_dens", "l_glyphs2D", "l_points", "l_rug"),
-                "Check2DFactorNumeric" = c("l_gridCheck2D", "l_points", "l_rug"),
-                "Check2DFactorFactor" = c("l_gridCheck2D", "l_points", "l_rug"),
-
-                message(paste("No layers available for objects of type", paste(ty, collapse = ' '))))
+  lay <- ls(getNamespace("mgcViz"), all.names=TRUE)
+  lay <- lay[ startsWith(lay, "l_") & endsWith(lay, paste0(".", ty)) ]
   
-  return(out)
+  if( length(lay) == 0 ) {  
+    message(paste0("No layers available for objects of type", ty)) 
+    lay <- NULL
+  } else {
+    lay <- gsub(paste0(".", ty), '', lay)
+  }
+  
+  return(lay)
 
 } 
