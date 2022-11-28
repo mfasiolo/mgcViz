992c3a6b5455f15fb3457d24697db0a959474c9e
Matteo Fasiolo <mf15002@it031161.wks.bris.ac.uk>
2022-11-28 13:51:30 +0000

    Removed plot.si

--- R/plot_si_smooth_1D.R
+++ R/plot_si_smooth_1D.R
@@ -17,17 +17,14 @@
 #' @return An object of class \code{c("plotSmooth", "gg")}.
 #' @name plot.si.smooth.1D
 #' @rdname plot.si.smooth.1D
-#' @export plot.si.smooth.1D
-#' @export
+# @export plot.si.smooth.1D
+# @export
 #' 
 plot.si.smooth.1D <- function(x, n = 100, xlim = NULL, maxpo = 1e4, trans = identity, inner = FALSE, ...)  {
   
   if( inner ){
     # 1) Prepare data
-    P <- .prepareInnerSI(o = x, n = n, xlim = xlim, ...)
-    
-    # 2) Produce output object
-    out <- .plot.si.smooth.1D.inner(x = P$smooth, P = P, trans = trans, maxpo = maxpo)
+    P <- .prepareInnerSi(o = x, n = n, xlim = xlim, ...)
     
   } else {
     # 1) Prepare data
@@ -72,20 +69,4 @@ plot.si.smooth.1D <- function(x, n = 100, xlim = NULL, maxpo = 1e4, trans = iden
     theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
   
   return( list("ggObj" = .pl, "data" = .dat, type = c("singleIndex", "1D")) )
-}
-
-########################
-#' @noRd
-.plot.si.smooth.1D.inner <- function(x, P, trans, maxpo) {
-  
-  .dat <- list()
-  
-  .dat$fit <- data.frame(x = as.factor(P$x), y = P$fit, ty = trans(P$fit), se = P$se)
-  .dat$misc <- list(trans = trans)
-  
-  .pl <- ggplot(data = .dat$fit, mapping = aes(x = x, y = y)) + 
-    labs(title = P$main, x = P$xlab, y = P$ylab) + theme_bw() + 
-    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
-  
-  return( list("ggObj" = .pl, "data" = .dat, type = c("singleIndexInner", "1D")) )
 }
