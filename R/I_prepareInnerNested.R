800bffe387b7a7afdcba5b72c4affab17f10e5ac
xuan-zhan-0417 <er25922@bristol.ac.uk>
2026-02-27 16:16:27 +0000

    add visulization for type of si_nexp

--- R/I_prepareInnerNested.R
+++ R/I_prepareInnerNested.R
@@ -1,94 +1,106 @@
-
 ##########
 # Internal method
-#
-.prepareInnerNested <- function(o, n, xlim, ylim = NULL, ...){
-  
-  if(!exists("expsmooth") || !exists("mgks") ){
-    expsmooth <- mgks <- function(x){}
+##########
+.prepareInnerNested <- function(o,
+                                n,
+                                xlim,
+                                ylim = NULL,
+                                smooth = FALSE,
+                                ...) {
+  if (!exists("expsmooth") || !exists("mgks")) {
+    expsmooth <- mgks <- function(x) {
+      
+    }
     stop("Please install the gamFactory package.")
   }
   
   gObj <- o$gObj
-  sm <- gObj$smooth[[ o$ism ]]
+  sm <- gObj$smooth[[o$ism]]
   
   si <- sm$xt$si
-  alpha <- si$alpha 
+  alpha <- si$alpha
   B <- si$B
   
-  da <- length( alpha )
+  da <- length(alpha)
   prange <- (sm$first.para:sm$last.para)[1:da]
   type <- class(o)[1]
   
   # Remove scale parameter of inner transformation
-  if( type %in% c("mgks", "nexpsm")){
+  if (type %in% c("mgks", "nexpsm")) {
     prange <- prange[-1]
-    da <- da-1
+    da <- da - 1
     alpha <- alpha[-1]
-    if(is.null(B)){
+    if (is.null(B)) {
       B <- diag(1, nrow = length(alpha))
     }
   }
   
   Va <- gObj$Vp[prange, prange, drop = FALSE]
   
-  # =========================================================
-  # 根据不同的嵌套模型类型 (type) 准备内层绘图数据
-  # =========================================================
-  if( type == "si_nexpsm" ){
-    # ---------------------------------------------------------
-    # 1. 双重嵌套模型 (si_nexpsm)
-    # ---------------------------------------------------------
-    # 获取该平滑项对应的所有参数的协方差矩阵 (内层+外层，与 jacobian 维度匹配)
-    prange_all <- sm$first.para:sm$last.para
-    Va_all <- gObj$Vp[prange_all, prange_all, drop = FALSE]
-    
-    n_nexp <- si$n_nexp
-    n_si <- si$n_si
-    
-    # 准备 alpha_nexp
-    idx_nexp <- 1:n_nexp
-    alpha_nexp <- si$alpha_nexp
-    Va_nexp <- Va_all[idx_nexp, idx_nexp, drop = FALSE]
-    if(!is.null(si$B_nexp)){
+  if (type == "si_nexpsm") {
+    if (!smooth) {
+      # coef plot
+      alpha_center <- si$alpha_center
+      alpha_si <- si$alpha_si
+      alpha_nexp <- si$alpha_nexp
+      n_si <- si$n_si
+      n_nexp <- si$n_nexp
+      
+      if (is.null(alpha_center)) {
+        alpha_center <- alpha_si * 0
+      }
+      
+      alpha_si <- drop(si$B_si %*% (alpha_si + alpha_center))
       alpha_nexp <- drop(si$B_nexp %*% alpha_nexp)
-      Va_nexp <- si$B_nexp %*% Va_nexp %*% t(si$B_nexp)
+      alpha <- c(alpha_nexp, alpha_si)
+      
+      Va_nexp <- si$B_nexp %*% Va[1:n_nexp, 1:n_nexp, drop = FALSE] %*% t(si$B_nexp)
+      Va_si <- si$B_si %*% Va[(n_nexp + 1):(n_nexp + n_si), (n_nexp + 1):(n_nexp + n_si), drop = FALSE] %*% t(si$B_si)
+      se_si <- sqrt(pmax(0, diag(Va_si)))
+      se_nexp <- sqrt(pmax(0, diag(Va_nexp)))
+      se <- c(se_nexp, se_si)
+      edf   <- sum(gObj$edf[prange])
+      ylabel <- .subEDF(paste0("Inner_coef(", sm$term, ")"), edf)
+      xlabel <- "Index (NEXP + SI)"
+      
+      out <- list(
+        fit = unname(alpha),
+        x = 1:da,
+        se = se,
+        xlab = xlabel,
+        ylab = ylabel,
+        main = NULL,
+        type = "si_nexpsm_coef"
+      )
+    } else {
+      # smooth eff plot
+      xa <- sm$xt$xa #values after smooth
+      times <- 1:length(xa)
+      edf   <- sum(gObj$edf[prange])
+      ylabel <- .subEDF(paste0("expsm(", sm$term, ")"), edf)
+      xlabel <- "Index"
+      
+      out <- list(
+        fit = xa,
+        x = times,
+        xlab = xlabel,
+        ylab = ylabel,
+        main = NULL,
+        type = "si_nexpsm_xa"
+      )
     }
-    se_nexp <- sqrt(pmax(0, diag(Va_nexp)))
-    
-    # 准备 alpha_si
-    idx_si <- (n_nexp + 1):(n_nexp + n_si)
-    alpha_si <- si$alpha_si
-    Va_si <- Va_all[idx_si, idx_si, drop = FALSE]
-    if(!is.null(si$B_si)){
-      alpha_si <- drop(si$B_si %*% alpha_si)
-      Va_si <- si$B_si %*% Va_si %*% t(si$B_si)
-    }
-    se_si <- sqrt(pmax(0, diag(Va_si)))
-    
-    # 准备 xa 的轨迹
-    xa <- sm$xt$xa
-    times <- 1:length(xa)
-    
-    out <- list(
-      type = "si_nexpsm",
-      main = NULL,
-      si = list(fit = alpha_si, x = 1:length(alpha_si), se = se_si,
-                xlab = "SI Index", ylab = "Alpha SI Weights", main = "Single Index Weights"),
-      nexp = list(fit = alpha_nexp, x = 1:length(alpha_nexp), se = se_nexp,
-                  xlab = "NEXP Index", ylab = "Alpha NEXP Weights", main = "Exp Smooth Parameters"),
-      xa = list(fit = xa, x = times,
-                xlab = "Index(Time)", ylab = "s_t", main = "After Smooth")
+  }
+  
+  if (type == "nexpsm") {
+    inner <- expsmooth(
+      y = si$x,
+      Xi = si$X,
+      beta = alpha,
+      deriv = 1
     )
-    
-  } else if( type == "nexpsm" ){
-    # ---------------------------------------------------------
-    # 2. 指数平滑模型 (nexpsm)
-    # ---------------------------------------------------------
-    inner <- expsmooth(y = si$x, Xi = si$X, beta = alpha, deriv = 1)
     fit <- inner$d0
     Jac <- inner$d1
-    if( !is.null(si$times) ){
+    if (!is.null(si$times)) {
       fit <- fit[1:max(si$times)]
       Jac <- Jac[1:max(si$times), ]
     }
@@ -98,7 +110,7 @@
     ylabel <- .subEDF(paste0("expsm(", sm$term, ")"), edf)
     xlabel <- "Index"
     
-    if( !is.null(xlim) ) {
+    if (!is.null(xlim)) {
       xlim <- sort(xlim)
       xlim[1] <- max(xlim[1], 1)
       xlim[2] <- min(xlim[2], nobs)
@@ -109,17 +121,24 @@
       ii <- 1:nobs
     }
     
-    out <- list("fit" = fit[ii], "x" = ii, "se" = se[ii],
-                "p.resid" = si$x[ii], "raw" = ii, 
-                "xlim" = xlim, 
-                xlab = xlabel, ylab = ylabel, main = NULL, type = "nexpsm")
+    out <- list(
+      "fit" = fit[ii],
+      "x" = ii,
+      "se" = se[ii],
+      "p.resid" = si$x[ii],
+      "raw" = ii,
+      "xlim" = xlim,
+      xlab = xlabel,
+      ylab = ylabel,
+      main = NULL,
+      type = "nexpsm"
+    )
     
-  } else if( type == "si" ) {
-    # ---------------------------------------------------------
-    # 3. 单指数模型 (si)
-    # ---------------------------------------------------------
+  }
+  
+  if (type == "si") {
     a0 <- si$a0
-    if( is.null(a0) ){
+    if (is.null(a0)) {
       a0 <- alpha * 0
     }
     alpha <- drop(B %*% (alpha + a0))
@@ -129,43 +148,45 @@
     ylabel <- .subEDF(paste0("Inner_coef(", sm$term, ")"), edf)
     xlabel <- "Index"
     
-    out <- list("fit" = alpha, "x" = 1:da, "se" = se,
-                xlab = xlabel, ylab = ylabel, main = NULL, type = "si")
+    out <- list(
+      "fit" = alpha,
+      "x" = 1:da,
+      "se" = se,
+      xlab = xlabel,
+      ylab = ylabel,
+      main = NULL,
+      type = "si"
+    )
     
-  } else {
-    # ---------------------------------------------------------
-    # 4. 未知类型兜底 / 保留给未来激活的 mgks
-    # ---------------------------------------------------------
-    # NOT CLEAR HOW TO DO THIS WITH DISTANCEs
-    # if( type == "mgks" ){
-    #   d <- ncol(si$X0)
-    #   if( d != 2 ){ return( NULL ) }
-    #   
-    #   if( !is.null(xlim) ) xlim <- sort(xlim) else xlim <- range(si$X[ , 1])
-    #   if( !is.null(ylim) ) ylim <- sort(ylim) else ylim <- range(si$X[ , 2])
-    #   
-    #   xx <- rep(seq(xlim[1], xlim[2], length.out = n), n)
-    #   yy <- rep(seq(ylim[1], ylim[2], length.out = n), rep(n, n))
-    #   X <- cbind(xx, yy)
-    #   
-    #   si$x <- as.matrix(si$x)
-    #   if( ncol(si$x) > 1 ){ si$x <- colMeans(si$x) }
-    #   
-    #   inner <- mgks(y = si$x, X = X, X0 = si$X0, beta = alpha[-1], deriv = 1)
-    #   fit <- inner$d0
-    #   Jac <- inner$d1
-    #   se <- sqrt(pmax(0, rowSums((Jac %*% Va[-1, -1, drop = FALSE]) * Jac)))
-    #   edf   <- sum(gObj$edf[prange[-1]])
-    #   
-    #   mainlab <- .subEDF(paste0("mgks(", sm$term, ")"), edf)
-    #   out <- list("fit" = fit, "X" = si$X, "se" = se, "x" = xx, "y" = yy,
-    #               "p.resid" = si$x, "X0" = si$X0, 
-    #               "xlim" = xlim, "ylim" = ylim,
-    #               "xlab" = "X[ , 1]", "ylab" = "X[ , 2]", "main" = mainlab, type = "mgks")
-    # } else {
-    stop("Unrecognized smooth effect type in .prepareInnerNested: ", type)
-    # }
   }
+  
+  # NOT CLEAR HOW TO DO THIS WITH DISTANCEs
+  # if( type == "mgks" ){
+  #   d <- ncol(si$X0)
+  #   if( d != 2 ){ return( NULL ) }
+  #
+  #   if( !is.null(xlim) ) xlim <- sort(xlim) else xlim <- range(si$X[ , 1])
+  #   if( !is.null(ylim) ) ylim <- sort(ylim) else ylim <- range(si$X[ , 2])
+  #
+  #   xx <- rep(seq(xlim[1], xlim[2], length.out = n), n)
+  #   yy <- rep(seq(ylim[1], ylim[2], length.out = n), rep(n, n))
+  #   X <- cbind(xx, yy)
+  #
+  #   si$x <- as.matrix(si$x)
+  #   if( ncol(si$x) > 1 ){ si$x <- colMeans(si$x) }
+  #
+  #   inner <- mgks(y = si$x, X = X, X0 = si$X0, beta = alpha[-1], deriv = 1)
+  #   fit <- inner$d0
+  #   Jac <- inner$d1
+  #   se <- sqrt(pmax(0, rowSums((Jac %*% Va[-1, -1, drop = FALSE]) * Jac)))
+  #   edf   <- sum(gObj$edf[prange[-1]])
+  #
+  #   mainlab <- .subEDF(paste0("mgks(", sm$term, ")"), edf)
+  #   out <- list("fit" = fit, "X" = si$X, "se" = se, "x" = xx, "y" = yy,
+  #               "p.resid" = si$x, "X0" = si$X0,
+  #               "xlim" = xlim, "ylim" = ylim,
+  #               "xlab" = "X[ , 1]", "ylab" = "X[ , 2]", "main" = mainlab, type = "mgks")
+  # }
   
   return(out)
   
--- R/plot_nested1D.R
+++ R/plot_nested1D.R
@@ -1,13 +1,15 @@
 #'
 #' Plotting one dimensional nested effects
-#' 
-#' @description This method should be used to plot smooth effects 
+#'
+#' @description This method should be used to plot smooth effects
 #'              of class \code{"si.smooth.1D"}.
 #' @param x a smooth effect object.
 #' @param inner if TRUE we are doing to plot the inner transformation, rather that then
-#'              outer smooth effect. 
-#' @param n number of grid points used to compute main effect and c.i. lines. 
-#'          For a nice smooth plot this needs to be several times the estimated degrees of 
+#'              outer smooth effect.
+#' @param smooth only suitable for type of "si_nexpsm",
+#'               if TRUE then plot data after inner transformation, rather than coefficients.
+#' @param n number of grid points used to compute main effect and c.i. lines.
+#'          For a nice smooth plot this needs to be several times the estimated degrees of
 #'          freedom for the smooth.
 #' @param xlim if supplied then this pair of numbers are used as the x limits for the plot.
 #' @param ylim if supplied then this pair of numbers are used as the y limits for the plot.
@@ -15,33 +17,52 @@
 #'              \code{resRug()} and \code{resPoints()}. If number of datapoints > \code{maxpo},
 #'              then a subsample of \code{maxpo} points will be taken.
 #' @param trans monotonic function to apply to the smooth and residuals, before plotting.
-#'              Monotonicity is not checked. 
+#'              Monotonicity is not checked.
 #' @param ... currently unused.
 #' @return An object of class \code{c("plotSmooth", "gg")}.
 #' @name plot.nested1D
 #' @rdname plot.nested1D
 #' @export plot.nested1D
 #' @export
-#' 
-plot.nested1D <- function(x, inner = FALSE, n = 100, xlim = NULL, ylim = NULL, maxpo = 1e4, trans = identity,  ...)  {
-  
-  if( inner ){
+#'
+plot.nested1D <- function(x,
+                          inner = FALSE,
+                          smooth = FALSE,
+                          n = 100,
+                          xlim = NULL,
+                          ylim = NULL,
+                          maxpo = 1e4,
+                          trans = identity,
+                          ...)  {
+  if (inner) {
     # 1) Prepare data
-    P <- .prepareInnerNested(o = x, n = n, xlim = xlim, ylim = ylim, ...)
+    P <- .prepareInnerNested(
+      o = x,
+      n = n,
+      xlim = xlim,
+      ylim = ylim,
+      smooth = smooth,
+      ...
+    )
     
-    out <- .plot.inner.nested.smooth.1D(P = P, trans = trans, maxpo = maxpo)
-    
-    # 【新增】针对双重嵌套，如果返回的是图表List，直接 return 即可
-    if(!is.null(P) && P$type == "si_nexpsm"){
-      return(out)
-    }
+    out <- .plot.inner.nested.smooth.1D(P = P,
+                                        trans = trans,
+                                        maxpo = maxpo)
     
   } else {
     # 1) Prepare data
-    P <- .prepareOuterNested(o = x, n = n, xlim = xlim, ...)
+    P <- .prepareOuterNested(o = x,
+                             n = n,
+                             xlim = xlim,
+                             ...)
     
     # 2) Produce output object
-    out <- .plot.outer.nested.1D(x = P$smooth, P = P, trans = trans, maxpo = maxpo)
+    out <- .plot.outer.nested.1D(
+      x = P$smooth,
+      P = P,
+      trans = trans,
+      maxpo = maxpo
+    )
   }
   
   class(out) <- c("plotSmooth", "gg")
@@ -52,111 +73,134 @@ plot.nested1D <- function(x, inner = FALSE, n = 100, xlim = NULL, ylim = NULL, m
 ########################
 #' @noRd
 .plot.outer.nested.1D <- function(x, P, trans, maxpo) {
-  
   .dat <- list()
   
-  if ( !is.null(P$raw) ) {
+  if (!is.null(P$raw)) {
     # Construct data.frame of partial residuals
     res <- data.frame("x" = as.vector(P$raw))
     
     # Exclude residuals falling outside boundaries
-    .dat$res <- res[res$x >= P$xlim[1] & res$x <= P$xlim[2], , drop = FALSE]
+    .dat$res <- res[res$x >= P$xlim[1] &
+                      res$x <= P$xlim[2], , drop = FALSE]
     
-    # Sample if too many points (> maxpo)  
-    nres <- nrow( .dat$res )
-    .dat$res$sub <- if(nres > maxpo) { 
-      sample( c(rep(T, maxpo), rep(F, nres-maxpo)) )
-    } else { 
-      rep(T, nres) 
+    # Sample if too many points (> maxpo)
+    nres <- nrow(.dat$res)
+    .dat$res$sub <- if (nres > maxpo) {
+      sample(c(rep(T, maxpo), rep(F, nres - maxpo)))
+    } else {
+      rep(T, nres)
     }
   }
   
-  .dat$fit <- data.frame(x = P$x, y = P$fit, ty = trans(P$fit), se = P$se)
+  .dat$fit <- data.frame(
+    x = P$x,
+    y = P$fit,
+    ty = trans(P$fit),
+    se = P$se
+  )
   .dat$misc <- list(trans = trans)
   
-  .pl <- ggplot(data = .dat$fit, mapping = aes(x = x, y = y)) + 
-    labs(title = P$main, x = P$xlab, y = P$ylab) + theme_bw() + 
-    theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+  .pl <- ggplot(data = .dat$fit, mapping = aes(x = x, y = y)) +
+    labs(title = P$main,
+         x = P$xlab,
+         y = P$ylab) + theme_bw() +
+    theme(panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank())
   
-  return( list("ggObj" = .pl, "data" = .dat, type = "1D") )
+  return(list(
+    "ggObj" = .pl,
+    "data" = .dat,
+    type = "1D"
+  ))
 }
 
 ########################
 #' @noRd
 .plot.inner.nested.smooth.1D <- function(P, trans, maxpo) {
+  if (is.null(P)) {
+    return(NULL)
+  }
   
-  # 空值检查 (提早返回是合理的)
-  if( is.null(P) ) { return(NULL) }
-  
-  # =========================================================
-  # 根据不同的嵌套模型类型 (type) 构建内层 ggplot 对象
-  # =========================================================
-  if( P$type == "si_nexpsm" ){
-    # ---------------------------------------------------------
-    # 1. 双重嵌套模型 (si_nexpsm) - 返回 3 个独立图表的 List
-    # ---------------------------------------------------------
-    build_coef_plot <- function(subP) {
-      .dat <- list()
-      .dat$fit <- data.frame("x"  = as.factor(subP$x),
-                             "y"  = unname(subP$fit),
-                             "ty" = trans(unname(subP$fit)),
-                             "se" = unname(subP$se))
-      .dat$misc <- list("trans" = trans)
-      
-      .pl <- ggplot(data = .dat$fit, aes("x" = x, "y" = ty)) +
-        labs(title = subP$main, x = subP$xlab, y = subP$ylab) +
-        theme_bw() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
-      
-      return(structure(list("ggObj" = .pl, "data" = .dat, "type" = c("si", "Factor")), 
-                       class = c("plotSmooth", "gg")))
-    }
+  if (P$type == "si_nexpsm_coef") {
+    # plot coef [alpha_nexp, alpha_si]
+    .dat <- list()
+    .dat$fit <- data.frame(
+      "x"  = P$x,
+      "y"  = P$fit,
+      "ty" = trans(P$fit),
+      "se" = P$se
+    )
+    .dat$misc <- list("trans" = trans)
     
-    build_xa_plot <- function(subP) {
-      .dat <- list()
-      .dat$fit <- data.frame("x" = subP$x, 
-                             "y" = subP$fit, 
-                             "ty" = trans(subP$fit))
-      .dat$misc <- list("trans" = trans)
-      
-      .pl <- ggplot(data = .dat$fit, aes(x = x, y = y)) + 
-        labs(title = subP$main, x = subP$xlab, y = subP$ylab) + 
-        theme_bw() + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
-      
-      return(structure(list("ggObj" = .pl, "data" = .dat, "type" = "1D"), 
-                       class = c("plotSmooth", "gg")))
-    }
+    .pl <- ggplot(data = .dat$fit, aes(x = x, y = ty)) +
+      labs(title = P$main,
+           x = P$xlab,
+           y = P$ylab) +
+      theme_bw() +
+      theme(panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank())
     
     out <- list(
-      plot_si   = build_coef_plot(P$si),
-      plot_nexp = build_coef_plot(P$nexp),
-      plot_xa   = build_xa_plot(P$xa)
+      "ggObj" = .pl,
+      "data" = .dat,
+      "type" = c("si", "Factor")
     )
     
-  } else if( P$type == "nexpsm" ){
-    # ---------------------------------------------------------
-    # 2. 指数平滑模型 (nexpsm) - 调用通用的 1D 画图函数
-    # ---------------------------------------------------------
-    out <- .plot.mgcv.smooth.1D(x = NULL, P = P, trans = trans, maxpo = maxpo)
-    
-  } else if( P$type == "mgks" ){
-    # ---------------------------------------------------------
-    # 3. 二维距离核平滑 (mgks) - 包含残差子采样逻辑
-    # ---------------------------------------------------------
+  } else if (P$type == "si_nexpsm_xa") {
+    # plot data after smooth
     .dat <- list()
-    .dat$fit <- data.frame("z"  = drop( P$fit ),
-                           "tz" = drop( trans(P$fit) ),
-                           "x"  = rep(P$x, length(P$fit) / length(P$x)),
-                           "y"  = rep(P$y, each = length(P$fit) / length(P$x)),
-                           "se" = P$se)
+    .dat$fit <- data.frame(
+      "x"  = P$x,
+      "y"  = P$fit,
+      "ty" = trans(P$fit),
+      "se" = NA
+    )
+    .dat$misc <- list("trans" = trans)
     
-    P$raw <- data.frame(z = P$p.resid, x = P$X0[ , 1], y = P$X0[ , 2])
-    if( !is.null(P$raw) ){
+    .pl <- ggplot(data = .dat$fit, aes(x = x, y = y)) +
+      labs(title = P$main,
+           x = P$xlab,
+           y = P$ylab) +
+      theme_bw() + theme(panel.grid.major = element_blank(),
+                         panel.grid.minor = element_blank())
+    
+    out <- list("ggObj" = .pl,
+                "data" = .dat,
+                "type" = "1D")
+    
+  }
+  
+  if (P$type == "nexpsm") {
+    out <- .plot.mgcv.smooth.1D(
+      x = NULL,
+      P = P,
+      trans = trans,
+      maxpo = maxpo
+    )
+    
+  }
+  
+  if (P$type == "mgks") {
+    .dat <- list()
+    .dat$fit <- data.frame(
+      "z"  = drop(P$fit),
+      "tz" = drop(trans(P$fit)),
+      "x"  = rep(P$x, length(P$fit) / length(P$x)),
+      "y"  = rep(P$y, each = length(P$fit) / length(P$x)),
+      "se" = P$se
+    )
+    
+    P$raw <- data.frame(z = P$p.resid,
+                        x = P$X0[, 1],
+                        y = P$X0[, 2])
+    if (!is.null(P$raw)) {
       .dat$res <- P$raw[P$raw$x >= P$xlim[1] & P$raw$x <= P$xlim[2] &
-                          P$raw$y >= P$ylim[1] & P$raw$y <= P$ylim[2] , , drop = FALSE]
+                          P$raw$y >= P$ylim[1] &
+                          P$raw$y <= P$ylim[2] , , drop = FALSE]
       
-      nres <- nrow( .dat$res )
-      .dat$res$sub <- if(nres > maxpo) {
-        sample( c(rep(TRUE, maxpo), rep(FALSE, nres-maxpo)) )
+      nres <- nrow(.dat$res)
+      .dat$res$sub <- if (nres > maxpo) {
+        sample(c(rep(TRUE, maxpo), rep(FALSE, nres - maxpo)))
       } else {
         rep(TRUE, nres)
       }
@@ -165,38 +209,47 @@ plot.nested1D <- function(x, inner = FALSE, n = 100, xlim = NULL, ylim = NULL, m
     .dat$misc <- list("trans" = trans)
     
     .pl <- ggplot(data = .dat$fit, aes(x = x, y = y, z = z)) +
-      labs(title = P$main, x = P$xlab, y = P$ylab) +
+      labs(title = P$main,
+           x = P$xlab,
+           y = P$ylab) +
       theme_bw() +
-      theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+      theme(panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank())
     
-    out <- list("ggObj" = .pl, "data" = .dat, "type" = c("mgks", "2D"))
+    out <- list(
+      "ggObj" = .pl,
+      "data" = .dat,
+      "type" = c("mgks", "2D")
+    )
     
-  } else if( P$type == "si" ){
-    # ---------------------------------------------------------
-    # 4. 单指数模型 (si) - 默认绘制因子图
-    # ---------------------------------------------------------
+  }
+  
+  if (P$type == "si") {
     .dat <- list()
-    .dat$fit <- data.frame("x"  = as.factor(P$x),
-                           "y"  = unname(P$fit),
-                           "ty" = trans( unname(P$fit) ),
-                           "se" = unname(P$se) )
+    .dat$fit <- data.frame(
+      "x"  = as.factor(P$x),
+      "y"  = unname(P$fit),
+      "ty" = trans(unname(P$fit)),
+      "se" = unname(P$se)
+    )
     .dat$misc <- list("trans" = trans)
     
     .pl <- ggplot(data = .dat$fit, aes("x" = x, "y" = ty)) +
-      labs(title = P$main, x = P$xlab, y = P$ylab) +
+      labs(title = P$main,
+           x = P$xlab,
+           y = P$ylab) +
       theme_bw() +
-      theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank()) 
+      theme(panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank())
     
-    out <- structure(list("ggObj" = .pl, "data" = .dat, "type" = c("si", "Factor")), 
-                     class = c("plotSmooth",  "gg"))
+    out <- structure(list(
+      "ggObj" = .pl,
+      "data" = .dat,
+      "type" = c("si", "Factor")
+    ),
+    class = c("plotSmooth", "gg"))
     
-  } else {
-    # ---------------------------------------------------------
-    # 5. 兜底保护
-    # ---------------------------------------------------------
-    stop("Unrecognized smooth effect type in .plot.inner.nested.smooth.1D: ", P$type)
   }
   
-  # 统一返回
   return(out)
 }
